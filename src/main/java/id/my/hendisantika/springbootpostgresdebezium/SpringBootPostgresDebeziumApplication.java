e03903a0347c1d4ae5d97f35500a9fd7bec432c0
hendisantika <hendisantika@yahoo.co.id>
2024-08-26 19:45:18 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootpostgresdebezium/SpringBootPostgresDebeziumApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootpostgresdebezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresDebeziumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgresDebeziumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootpostgresdebezium/SpringBootPostgresDebeziumApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootpostgresdebezium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresDebeziumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
