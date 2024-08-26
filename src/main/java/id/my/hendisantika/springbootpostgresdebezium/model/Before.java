70dcdb731c3830b0ee1ad51f7a820c2a5da37b9d
hendisantika <hendisantika@yahoo.co.id>
2024-08-26 19:48:05 +0700

    Add Before Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootpostgresdebezium/model/Before.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.springbootpostgresdebezium.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgres-debezium
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/08/24
+ * Time: 19.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Before {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("policy_number")
+    private String policyNumber;
+
+    @JsonProperty("policy_holder")
+    private String policyHolder;
+
+    @JsonProperty("start_date")
+    private Long startDate;
+
+    @JsonProperty("end_date")
+    private Long endDate;
+}
