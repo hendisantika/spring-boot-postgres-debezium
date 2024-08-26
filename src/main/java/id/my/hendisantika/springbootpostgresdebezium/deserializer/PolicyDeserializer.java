b1be851f25cf88ef63aa0fdefc26ffb7fdb54b7e
hendisantika <hendisantika@yahoo.co.id>
2024-08-26 19:52:26 +0700

    Add PolicyDeserializer 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootpostgresdebezium/deserializer/PolicyDeserializer.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbootpostgresdebezium.deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.springbootpostgresdebezium.model.PolicyMessageCDC;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Deserializer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgres-debezium
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/08/24
+ * Time: 19.51
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+public class PolicyDeserializer implements Deserializer<PolicyMessageCDC> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+}
