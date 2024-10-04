cd32f0eb10e6905b705edd3cd623685f048ee797
hendisantika <hendisantika@yahoo.co.id>
2024-10-04 09:59:19 +0700

    feat: Add Test Containers🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/springbootpostgresdebezium/SpringBootPostgresDebeziumApplicationTests.java
+++ src/test/java/id/my/hendisantika/springbootpostgresdebezium/SpringBootPostgresDebeziumApplicationTests.java
@@ -1,13 +1,28 @@
 package id.my.hendisantika.springbootpostgresdebezium;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.springbootpostgresdebezium.repository.PolicyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:postgresql:17.0-alpine3.19:///debezium"
+        },
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class SpringBootPostgresDebeziumApplicationTests {
 
-    @Test
-    void contextLoads() {
+
+    @Autowired
+    private PolicyRepository policyRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        policyRepository.deleteAll();
     }
 
 }
